80be90ad46e9be0954b83eea3ccdfced9ab3245d
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-10-24 10:25:51 +0800

    Edited about info

--- src/js/About.js
+++ src/js/About.js
@@ -7,10 +7,10 @@ function About() {
     <div className="bg-[#DA9B2A] p-14">
       <h1 className="text-3xl font-bold py-2 pb-7 text-center">About the Test</h1>
       <p className="text-center px-10 pb-2">
-      Dolor nostrud proident elit mollit ex occaecat lorem ullamco et tempor amet laborum dolore non ullamco cillum id enim. Officia cupidatat
-      magna exercitation nisi pariatur magna quis ea mollit ex sed sint consequat laborum excepteur. Eiusmod labore esse et mollit mollit proident
-      deserunt ullamco deserunt aliquip ex fugiat veniam ullamco laborum. Cupidatat ad sed culpa consectetur laboris amet deserunt exercitation
-      eiusmod cillum sint dolore do sunt duis.
+      SkinGenius is an advanced skin assessment tool distinguished by its meticulous design and thorough professional review carried out by a
+      doctor affiliated with Scire Essentials. The diagnostic test delves deep into the subtleties of your skin type, offering an accurate and
+      personalized skincare profile. The blend of expert programming and rigorous evaluation ensures that the results are both precise and
+      reliable, allowing you to embark on a skincare journey that is tailor-made for your unique skin needs.
       </p>
     </div>
   );
