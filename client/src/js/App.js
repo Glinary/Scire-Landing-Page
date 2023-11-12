523ce4b6aab9405dc6d828a457004a367f243ca4
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-13 03:57:02 +0800

    Edited website structure

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,7 +1,5 @@
 import "../css/App.css";
 import Main from "./Main";
-import Header from "./Header";
-import Footer from "./Footer";
 import About from "./About";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -12,11 +10,8 @@ import "@fontsource/roboto/700.css";
 function App() {
   return (
     <div className="bg-[#FDFAF3]">
-      <Header />
       <Main />
-
       <About />
-      <Footer />
     </div>
   );
 }
