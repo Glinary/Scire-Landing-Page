1f51abd520183ed69cae40c2fde9d0526338d8d6
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-18 18:17:48 +0800

    Added more styling for picture and text alignment

--- client/src/js/FeaturedProducts.js
+++ client/src/js/FeaturedProducts.js
@@ -16,35 +16,35 @@ function FeaturedProducts() {
 
             <div className="px-14 mb-14">
                 <div className="flex border border-emerald-900 rounded-md mb-8 h-full px-8 py-4 space-x-6"> {/* Product 1 */}
-                    <img src={oryol_1} alt="oryol-1" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <img src={oryol_2} alt="oryol-2" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <div className="basis-1/2 h-full">
+                    <img src={oryol_1} alt="oryol-1" className="basis-1/4 object-cover h-contain overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <img src={oryol_2} alt="oryol-2" className="basis-1/4 object-cover h-contain overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <div className="basis-1/2 h-full py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Oryol</p>
                         <p className="text-lg">Elemi Rejuvenating Bar Soap</p>
                         <p className="text-md pb-12">Exfoliating, Firming, Lifting, Anti-acne, Hydrating</p>
-                        <p className="text-md">Ingredients: Aqua, Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Salicylic Acid, Elemi Resinoid</p>
+                        <p className="text-sm">Ingredients: Aqua, Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Salicylic Acid, Elemi Resinoid</p>
                     </div>
                 </div>
                 
                 <div className="flex border border-emerald-900 rounded-md mb-8 h-full px-8 py-4 space-x-6"> {/* Product 2 */}
-                    <img src={magayon_1} alt="magayon-1" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <img src={magayon_2} alt="magayon-2" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <div className="basis-1/2 h-full">
+                    <img src={magayon_1} alt="magayon-1" className="basis-1/4 object-cover overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <img src={magayon_2} alt="magayon-2" className="basis-1/4 object-cover overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <div className="basis-1/2 h-full py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Magayon</p>
                         <p className="text-lg">Elemi Glow Bar Soap</p>
                         <p className="text-md pb-12">Firming, Lifting, Detoxifying, Mild Exfoliating, Hydrating</p>
-                        <p className="text-md">Ingredients: Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Elemi Resinoid, Water</p>
+                        <p className="text-sm">Ingredients: Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Elemi Resinoid, Water</p>
                     </div>
                 </div>
                 
                 <div className="flex border border-emerald-900 rounded-md h-full px-8 py-4 space-x-6"> {/* Product 3 */}
-                    <img src={handyong_1} alt="handyong-1" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <img src={handyong_2} alt="handyong-2" className="basis-1/4 object-cover h-full overflow-hidden p-4 border border-[#aaaaaa]"></img>
-                    <div className="basis-1/2 h-full">
+                    <img src={handyong_1} alt="handyong-1" className="basis-1/4 object-cover overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <img src={handyong_2} alt="handyong-2" className="basis-1/4 object-cover overflow-hidden p-4 border border-[#aaaaaa]"></img>
+                    <div className="basis-1/2 h-ful py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Handyong</p>
-                        <p className="text-lg pb-12">Elemi Fresh Bar Soap</p>
-                        <p className="text-md">Firming, Lifting, Refreshing, Anti-bacterial, Hydrating</p>
-                        <p className="text-md">Ingredients: Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Tea Tree Oil, Peppermint Oil, Elemi Resinoid, Water</p>
+                        <p className="text-lg">Elemi Fresh Bar Soap</p>
+                        <p className="text-md pb-12">Firming, Lifting, Refreshing, Anti-bacterial, Hydrating</p>
+                        <p className="text-sm">Ingredients: Palm Oil, Coconut Oil, Olive Oil, Lye, Aloe Vera, Elemi Oil, Tea Tree Oil, Peppermint Oil, Elemi Resinoid, Water</p>
                     </div>
                 </div>
             </div>
