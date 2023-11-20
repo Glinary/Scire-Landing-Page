315b177bcb66e7544ad03055de4845011b45ec09
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-20 23:37:03 +0800

    Added styling responsiveness for header

--- client/src/js/Header.js
+++ client/src/js/Header.js
@@ -7,22 +7,22 @@ import { theme } from "./theme";
 
 const Header = () => {
   return (
-    <div className="flex flex-col items-center pt-20 pb-7">
-      <div id="header-whitespace" className="h-16"></div>
+    <div className="flex flex-col items-center pt-20 pb-7 2xl:pt-28 min-[300px]:pt-48 max-[700px]:pb-10">
+      <div id="header-whitespace" className="h-16 md:h-12"></div>
       
-      <h1 className="text-6xl py-1" id="header">SkinGenius</h1>
-      <p className="md:text-2xl text-base pb-1" id="header">
+      <h1 className="text-center text-6xl py-1 md:py-0.5 min-[300px]:text-5xl" id="header">SkinGenius</h1>
+      <p className="text-center text-2xl min-[700px]:text-1xl min-[700px]:pb-2 max-[700px]:text-base max-[700px]:pb-5" id="header">
         The beginning of your skin care journey
       </p>
       
-      <p className="text-md md:text-xl text-center pt-5">
+      <p className="text-xl text-center pt-5 min-[700px]:text-lg max-[700px]:text-xs max-[700px]:px-10">
         Scire Essentials presents you a test to check your skin type and become{" "}
         <span className="hidden md:inline"><br/></span>
         aware which products and routine suits your skin better.
       </p>
 
       <div className="flex flex-row items-center justify-center">
-        <p className="text-md md:text-xl text-center py-6 pr-2">
+        <p className="text-center text-xl min-[700px]:text-lg min-[700px]:py-4 max-[700px]:text-xs py-6 pr-2">
           You may also try our Messenger chatbot here:
         </p>
 
