cb46b5f68a0dd077095731cd5363addf2f864572
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-21 01:07:42 +0800

    Fixed responsiveness for large devices

--- client/src/js/About.js
+++ client/src/js/About.js
@@ -8,7 +8,7 @@ function About() {
     <div id="about-container">
     <div className="p-14" id="about">
       <h1 className="text-3xl font-bold py-2 pb-7 text-center opacity-100" id="about-title">About the Test</h1>
-      <p className="text-center text-xs lg:text-sm lg:px-10 lg:pb-2 font-bold" id="about-desc">
+      <p className="text-center text-xs min-[1000px]:text-sm min-[700px]:px-12 min-[1440px]:px-32 lg:pb-2 font-bold" id="about-desc">
       SkinGenius is an advanced skin assessment tool distinguished by its meticulous design and thorough professional review carried out by a
       doctor affiliated with Scire Essentials. The diagnostic test delves deep into the subtleties of your skin type, offering an accurate and
       personalized skincare profile. The blend of expert programming and rigorous evaluation ensures that the results are both precise and
