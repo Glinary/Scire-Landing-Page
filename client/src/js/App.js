c4da47f0e169f90ab35f2fc33646d006659e0851
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-21 01:19:15 +0800

    Removed temporary placeholder

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -5,7 +5,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import FeaturedProducts from "./FeaturedProducts"
 
 // Set-up of the entire page
 function App() {
