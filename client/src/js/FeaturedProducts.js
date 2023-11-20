c4ec9f763811151340a7b00e66245b775cae5b39
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-11-20 13:56:16 +0800

    Fixed paddings

--- client/src/js/FeaturedProducts.js
+++ client/src/js/FeaturedProducts.js
@@ -12,12 +12,12 @@ function FeaturedProducts() {
 
     return (
         <div className="border-2 border-emerald-900 m-10 rounded-lg">
-            <h1 className="text-center text-2xl font-bold py-10">Featured Products</h1>
+            <h1 className="text-center text-2xl font-bold py-14">Featured Products</h1>
 
-            <div className="px-14 mb-14">
-                <div className="flex border border-emerald-900 rounded-md mb-8 h-full px-10 py-8 space-x-8"> {/* Product 1 */}
-                    <img src={oryol_1} alt="oryol-1" className="basis-1/4 object-cover h-contain overflow-hidden p-8 border border-[#aaaaaa]"></img>
-                    <img src={oryol_2} alt="oryol-2" className="basis-1/4 object-cover h-contain overflow-hidden p-8 border border-[#aaaaaa]"></img>
+            <div className="px-20 mb-14">
+                <div className="flex border border-emerald-900 rounded-md mb-10 h-full px-14 py-14 space-x-8"> {/* Product 1 */}
+                    <img src={oryol_1} alt="oryol-1" className="basis-1/4 object-cover h-contain overflow-hidden p-6 border border-[#aaaaaa]"></img>
+                    <img src={oryol_2} alt="oryol-2" className="basis-1/4 object-cover h-contain overflow-hidden p-6 border border-[#aaaaaa]"></img>
                     <div className="basis-1/2 h-full py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Oryol</p>
                         <p className="text-lg">Elemi Rejuvenating Bar Soap</p>
@@ -26,9 +26,9 @@ function FeaturedProducts() {
                     </div>
                 </div>
                 
-                <div className="flex border border-emerald-900 rounded-md mb-8 h-full px-10 py-8 space-x-8"> {/* Product 2 */}
-                    <img src={magayon_1} alt="magayon-1" className="basis-1/4 object-cover overflow-hidden p-8 border border-[#aaaaaa]"></img>
-                    <img src={magayon_2} alt="magayon-2" className="basis-1/4 object-cover overflow-hidden p-8 border border-[#aaaaaa]"></img>
+                <div className="flex border border-emerald-900 rounded-md mb-10 h-full px-14 py-14 space-x-8"> {/* Product 2 */}
+                    <img src={magayon_1} alt="magayon-1" className="basis-1/4 object-cover overflow-hidden p-6 border border-[#aaaaaa]"></img>
+                    <img src={magayon_2} alt="magayon-2" className="basis-1/4 object-cover overflow-hidden p-6 border border-[#aaaaaa]"></img>
                     <div className="basis-1/2 h-full py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Magayon</p>
                         <p className="text-lg">Elemi Glow Bar Soap</p>
@@ -37,9 +37,9 @@ function FeaturedProducts() {
                     </div>
                 </div>
                 
-                <div className="flex border border-emerald-900 rounded-md h-full px-10 py-8 space-x-8"> {/* Product 3 */}
-                    <img src={handyong_1} alt="handyong-1" className="basis-1/4 object-cover overflow-hidden p-8 border border-[#aaaaaa]"></img>
-                    <img src={handyong_2} alt="handyong-2" className="basis-1/4 object-cover overflow-hidden p-8 border border-[#aaaaaa]"></img>
+                <div className="flex border border-emerald-900 rounded-md h-full px-14 py-14 space-x-8"> {/* Product 3 */}
+                    <img src={handyong_1} alt="handyong-1" className="basis-1/4 object-cover overflow-hidden p-6 border border-[#aaaaaa]"></img>
+                    <img src={handyong_2} alt="handyong-2" className="basis-1/4 object-cover overflow-hidden p-6 border border-[#aaaaaa]"></img>
                     <div className="basis-1/2 h-ful py-4 align-middle">
                         <p className="text-2xl font-bold pb-1">Handyong</p>
                         <p className="text-lg">Elemi Fresh Bar Soap</p>
